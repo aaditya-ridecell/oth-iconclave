2b551fb09a1a55b384d77731cc6fb53b43e3c1c0
Yashodhan Agnihotri <work.yashodhaniiitp@gmail.com>
2020-01-30 23:33:46 +0530

    OTH folder commit

---
+++ base_app/treasurehunt/web_templates/php/action_page.php
@@ -0,0 +1,42 @@
+<?php
+$fullname = $_POST['fullname'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+if (!empty($fullname) || empty($username) || !empty($password) || !empty($email) || !empty($phone)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "youtube";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From register Where email = ? Limit 1";
+     $INSERT = "INSERT Into register (username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssssii", $username, $password, $gender, $email, $phoneCode, $phone);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
