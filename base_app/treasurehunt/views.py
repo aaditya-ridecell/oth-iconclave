245e365c3b168bcbeccd460d856b9d8c3e5c3b8c
unknown <aadityajoshi2699@gmail.com>
2020-01-31 18:13:23 +0530

    Authentication Added

--- base_app/treasurehunt/views.py
+++ base_app/treasurehunt/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from . import forms
 # Create your views here.
 from treasurehunt import models
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request, 'treasurehunt/index.html')
 
 
 def register(request):
@@ -34,3 +38,31 @@ def register(request):
         'user_form': user_form,
         'registered': registered
     })
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('treasurehunt:index'))
+            else:
+                return HttpResponse("ACCOUNT NOT ACTIVE")
+        else:
+            print("Someone tried to login and failed")
+            print("UserName : {} and password {} ".format(username, password))
+            return HttpResponse("Invalid Login Details")
+    else:
+        return render(request, 'treasurehunt/login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('treasurehunt:index'))
